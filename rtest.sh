c6789a1410e0fb85d240f912340af3529f36c4da
nori <a@a.a>
2021-04-04 16:34:43 +0200

    fix

--- rtest.sh
+++ rtest.sh
@@ -15,7 +15,7 @@ print_result() {
 		printf '\n\033[32m%s\033[m\n' '[OK]'
 		echo -e "============================================\n"
 	else
-		ko[k]=$i
+		ko[k]=$2
 		let k++
 		printf '\n\033[31m%s\033[m\n' '[KO]'
 		echo -e "============================================\n"
@@ -29,7 +29,7 @@ file_test() {
 	sleep 0.1
 	kill $! > /dev/null 2>&1
 	test "$?" $3 "0"
-	print_result $?
+	print_result $? $4
 	wait $! 2>/dev/null
 	let cases++
 }
@@ -40,16 +40,25 @@ echo -e "============================================\n"
 
 for i in {1..100}
 do
-	file_test $i.rt $path_ko !=
+	file_test $i.rt $path_ko != $i
 	FILE="$path_ko/$i.rt"
 	if [ ! -e $FILE ]; then
 		break
 	fi
 done
 
-file_test .rt $path_ko !=
-file_test a $path_ko !=
-file_test a.art $path_ko !=
+file_test .rt $path_ko != .rt
+file_test a $path_ko != a
+file_test a.art $path_ko != a.art
+
+segv=1
+echo "file : dir.rt"
+bash -c './miniRT rt/invalid/dir.rt > rtest1..rt 2>sanitize_res..rt' > Seg_fault..rt 2>&1
+strings sanitize_res..rt | grep SEGV > /dev/null || strings Seg_fault..rt | grep Segment > /dev/null || segv=0
+echo $segv
+print_result $segv dir
+rm rtest1..rt sanitize_res..rt Seg_fault..rt
+let cases++
 
 echo -e "\n============================================"
 echo "===================valid===================="
@@ -61,12 +70,12 @@ do
 	if [ ! -e $FILE ]; then
 		break
 	fi
-	file_test $i.rt $path_ok =
+	file_test $i.rt $path_ok = $i
 done
 
 cp $path_ok/--save.rt .
-file_test ..rt $path_ok =
-file_test --save.rt . =
+file_test ..rt $path_ok = ..rt
+file_test --save.rt . = --save.rt
 rm ./--save.rt
 
 ps
